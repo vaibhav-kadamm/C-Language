98cdd997fc4261665c08591a1fdb8b40c1244861
Vaibhav Kadam <109782024+vaibhav-kadamm@users.noreply.github.com>
2023-08-05 00:14:50 +0530

    Add files via upload

---
+++ 01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int num1=2;
+    int num2=3;
+    int sum=num1+num2;
+    printf("%d+%d=%d",num1,num2,sum);
+    scanf("%d,%d" , &num1,&num2);
+    return 0;
+}
