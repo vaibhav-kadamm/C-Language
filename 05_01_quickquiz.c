20e065c78de436dd45e5439ba23d4835b3884581
Vaibhav Kadam <109782024+vaibhav-kadamm@users.noreply.github.com>
2023-08-09 12:52:01 +0530

    Add files via upload

---
+++ 05_01_quickquiz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void GoodMorning();
+void GoodAfternoon();
+void GoodNight();
+
+int main(){
+    GoodMorning();
+    GoodAfternoon();
+    GoodNight();
+    
+}
+void GoodMorning(){
+    printf("Good Morning Vaibhav\n");
+}
+void GoodAfternoon(){
+    printf("Good Afternoon Vaibhav\n");
+}
+void GoodNight(){
+    printf("Good Night Vaibhav\n");
+}
