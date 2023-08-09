867ed68cd9c865b9d94dbbd89beb536187c15f50
Vaibhav Kadam <109782024+vaibhav-kadamm@users.noreply.github.com>
2023-08-09 12:51:20 +0530

    Add files via upload

---
+++ 05.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int principal =101;
+    int rate=4; 
+    int years=11;
+    int simpleinterest=(principal*rate*years);
+    printf("the value of simpleinterest is %d",simpleinterest);
+
+    return 0;
+}
