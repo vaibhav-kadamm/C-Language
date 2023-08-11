5f03f6c0fa73a8b47e7c5cf31fa0aa8e8fc20cdb
Vaibhav Kadam <109782024+vaibhav-kadamm@users.noreply.github.com>
2023-08-11 13:43:36 +0530

    Add files via upload

---
+++ 06_06_avearge_value.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void sumandavg(int a,int b,int *sum,float*avg)
+{
+    *sum=a+b;
+    *avg=(float)*sum/2;
+}
+int main(){
+    int i,j,sum;
+    float avg;
+    i=2;
+    j=4;
+    sumandavg(i,j,&sum,&avg);
+    printf("the value of sum is %d\n",sum);
+
+    printf("the value of avg is %f\n",avg);
+    
+    return 0;
+}
